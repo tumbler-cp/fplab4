a497c16bb9ae1acd2bd52d33d8663ef82dee7a65
arahnik <abdujalol04@mail.ru>
2025-02-15 10:10:00 +0300

    init

---
+++ Test/UnitTest1.fs
@@ -0,0 +1,27 @@
+﻿module Test
+
+open NUnit.Framework
+open GameOfLife
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let ``Test cell revival`` () =
+    let initialBoard = Set.empty
+    let newBoard = nextGeneration (Set.ofList [(1, 0); (1, 1); (1, 2)])
+    Assert.That(newBoard.Contains (1, 1))
+
+[<Test>]
+let ``Test cell removal`` () =
+    let initialBoard = Set.ofList [(1, 1); (1, 2); (1, 3)]
+    let newBoard = nextGeneration initialBoard
+    Assert.That(newBoard.Contains (1, 1), Is.False)
+
+[<Test>]
+let ``Test grid update`` () =
+    let initialBoard = Set.ofList [(1, 0); (1, 1); (1, 2)]
+    let expectedBoard = Set.ofList [(0, 1); (1, 1); (2, 1)]
+    let newBoard = nextGeneration initialBoard
+    Assert.That(newBoard, Is.EqualTo(expectedBoard))
